bd67ad780153dd722d1ea011ee64be3a0e98e829
Lars De Winter <lars@Larss-MacBook-Pro.local>
2019-11-24 19:15:09 +0100

    testing

--- plugins/miscellanea/lcd_controller/test.js
+++ plugins/miscellanea/lcd_controller/test.js
@@ -1,26 +1,21 @@
 'use strict';
 
-// This example demonstrates how to call print twice in succession to display
-// two strings at different positions on the LCD.
-
 const Lcd = require('lcd');
 const lcd = new Lcd({rs: 26, e: 24, data: [22, 18, 16, 12], cols: 20, rows: 4});
 
-
 lcd.on('ready', _ => {
-  lcd.setCursor(0, 0);
-  lcd.print('Hello!', err => {
-    if (err) {
-      throw err;
-    }
-
-    lcd.setCursor(0, 1);
-    lcd.print('How are you?', err => {
+  setInterval(_ => {
+    lcd.setCursor(0, 0);
+    lcd.print(new Date().toISOString().substring(11, 19), err => {
       if (err) {
         throw err;
       }
-
-      lcd.close();
     });
-  });
+  }, 1000);
+});
+
+// If ctrl+c is hit, free resources and exit.
+process.on('SIGINT', _ => {
+  lcd.close();
+  process.exit();
 });
