89360cc38515f5ef7e5176af68d9c665aafd3066
Lars De Winter <lars@Larss-MacBook-Pro.local>
2021-07-18 19:55:00 +0200

    t

--- plugins/miscellanea/lcdcontroller/LCDcontroller/main.py
+++ plugins/miscellanea/lcdcontroller/LCDcontroller/main.py
@@ -104,7 +104,7 @@ try:
         if(str(text_scroll) == "1"):
             # Value is 1, scroll the text if too long
             while(True):
-                print(info)
+                print(artist, album, trackType, status)
                 # Check for updates on the information we have
                 if(music_info.check_for_updates() !=  False):
                     sleep(0.5)
