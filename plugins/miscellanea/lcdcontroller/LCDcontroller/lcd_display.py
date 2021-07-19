2332e9a964d7f8f04c4691545e95be666c2b8d52
Lars De Winter <lars@Larss-MacBook-Pro.local>
2021-07-19 19:42:30 +0200

    Working version

--- plugins/miscellanea/lcdcontroller/LCDcontroller/lcd_display.py
+++ plugins/miscellanea/lcdcontroller/LCDcontroller/lcd_display.py
@@ -30,7 +30,7 @@ class lcd:
   def __init__(self):
     """Setup the display, turn on backlight and text display + ...?"""
     # Initialize the LCD using the pins above.
-    self.device = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight, 1, False)
+    self.device = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
     sleep(0.2)
 
   def strobe(self, data):
